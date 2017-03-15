de3b322966c8e44a48c172837013db88c7de3a0b
josefinamont <mariajosefina.mont@gmail.com>
2017-03-15 17:57:41 -0300

    Mas cambios

--- src/tp0.c
+++ src/tp0.c
@@ -9,29 +9,55 @@ int main(int argc, char *argv[]) {
 	struct option long_opt[] = {
 	      {"version", no_argument, NULL, 'V'},
 	      {"help", no_argument, NULL, 'h'},
-		  {"input", no_argument, NULL, 'i'},
-		  {"output", no_argument, NULL, 'o'},
+		  {"input", required_argument, NULL, 'i'},
+		  {"output", required_argument, NULL, 'o'},
 		  {"qsort", no_argument, NULL, 'q'},
 		  {"bsort", no_argument, NULL, 'b'},
-	      {NULL,            0,                 NULL, 0  }
+	      {NULL, 0, NULL, 0}
 	};
+	FILE* inputFile;
+	FILE* outputFile;
+	char sortMethod;
 
 	while ((option = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
 		switch(option) {
-			case 'b': /*if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-H")) {
-						printf("Usage: \n");
-						printf("	%s -h \n", argv[0]);
-						printf("	%s -V \n", argv[0]);
-						printf("	%s [options] file \n", argv[0]);
-						printf("Options: \n");
-						printf("	-V, --version		Print version and quit. \n");
-						printf("	-h, --help		Print this information. \n");
-						printf("	-o, --output		Path to output file. \n");
-						printf("	-i, --input		Path to input file. \n");
-						printf("	-q, --qsort		Use quicksort. \n");
-						printf("	-b, --bsort		Use bubblesort. \n");*/
-					printf("Hello \n");
-					break;
+			case 'V':
+				printf("TP0 de la materia Organización de Computadoras \n");
+				printf("Alumnos: \n");
+				printf("	Flórez Del Carpio Christian\n	Montenegro Josefina \n	Riego Leonardo \n");
+				break;
+			case 'h':
+				printf("Usage: \n");
+				printf("	%s -h \n", argv[0]);
+				printf("	%s -V \n", argv[0]);
+				printf("	%s [options] file \n", argv[0]);
+				printf("Options: \n");
+				printf("	-V, --version		Print version and quit. \n");
+				printf("	-h, --help		Print this information. \n");
+				printf("	-o, --output		Path to output file. \n");
+				printf("	-i, --input		Path to input file. \n");
+				printf("	-q, --qsort		Use quicksort. \n");
+				printf("	-b, --bsort		Use bubblesort. \n");
+				break;
+			case 'i':
+				inputFile = fopen(optarg, "r");
+				printf("Recibi archivo %s \n", optarg);
+				break;
+			case 'o':
+				outputFile = fopen(optarg, "w");
+				printf("Recibi archivo %s \n", optarg);
+				break;
+			case 'b':
+				sortMethod = option;
+				printf("%c \n", sortMethod);
+				break;
+			case 'q':
+				sortMethod = option;
+				printf("%c \n", sortMethod);
+				break;
+			default:
+			    printf ("Opción inválida. Para ver más información ingrese -h. \n");
+
 		}
 	}
 
