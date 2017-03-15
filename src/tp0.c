67b21fb4f80c242d7a091ce8757e2b01f7aa5cce
josefinamont <mariajosefina.mont@gmail.com>
2017-03-15 17:20:34 -0300

    Ahora se aceptan comandos largos

--- src/tp0.c
+++ src/tp0.c
@@ -5,9 +5,20 @@
 int main(int argc, char *argv[]) {
 
 	int option = 0;
-	while ((option = getopt(argc, argv,"qbi:o:hV")) != -1) {
+	const char* short_opt = "qbi:o:hV";
+	struct option long_opt[] = {
+	      {"version", no_argument, NULL, 'V'},
+	      {"help", no_argument, NULL, 'h'},
+		  {"input", no_argument, NULL, 'i'},
+		  {"output", no_argument, NULL, 'o'},
+		  {"qsort", no_argument, NULL, 'q'},
+		  {"bsort", no_argument, NULL, 'b'},
+	      {NULL,            0,                 NULL, 0  }
+	};
+
+	while ((option = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
 		switch(option) {
-			case 'b': if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-H")) {
+			case 'b': /*if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "-H")) {
 						printf("Usage: \n");
 						printf("	%s -h \n", argv[0]);
 						printf("	%s -V \n", argv[0]);
@@ -18,8 +29,8 @@ int main(int argc, char *argv[]) {
 						printf("	-o, --output		Path to output file. \n");
 						printf("	-i, --input		Path to input file. \n");
 						printf("	-q, --qsort		Use quicksort. \n");
-						printf("	-b, --bsort		Use bubblesort. \n");
-					}
+						printf("	-b, --bsort		Use bubblesort. \n");*/
+					printf("Hello \n");
 					break;
 		}
 	}
