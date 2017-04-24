956e0d826c37e41bf4ce06f5f358099e76ae4e40
ChrisFlorez <chris.florez.d.c@gmail.com>
2017-04-24 20:41:36 -0300

    Se suben los primeros 3 programas con el comando a ejecutar y el codigo del cuarto sin resolver

---
+++ tp1/stack1.c
@@ -0,0 +1,22 @@
+/*
+* stack1-stdin.c
+*
+* specially crafted to feed your brain by gera */
+
+#include <stdio.h>
+
+int main() {
+
+	char buf[80];
+	int cookie=1;
+	printf("buf: %08x cookie: %08x\n", &buf, &cookie);
+	gets(buf);
+	printf ("cookie: %08x\n", cookie);
+	if (cookie == 0x41424344)
+		printf("you win!\n");
+}
+
+/*
+*compilar el programa, y luego ejecutar el siguiente comando:
+* echo -e "AAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDDEEEEEEEEEEFFFFFFFFFFGGGGGGGGGGHHHHHHHHHHDCBA"|./stack1
+*/ 
---
+++ tp1/stack2.c
@@ -0,0 +1,20 @@
+/* stack2-stdin.c
+*
+* specially crafted to feed your brain by gera */
+
+#include <stdio.h>
+
+int main() {
+	char buf[80];
+	int cookie;
+	printf("buf: %08x cookie: %08x\n", &buf, &cookie);
+	gets(buf);
+	printf("cookie: %08x\n", cookie);
+	if (cookie == 0x01020305)
+		printf("you win!\n");
+}
+
+/*
+*compilar el programa, y luego ejecutar el siguiente comando:
+* echo -e "AAAAAAAAAABBBBBBBBBBCCCCCCCCCCDDDDDDDDDDEEEEEEEEEEFFFFFFFFFFGGGGGGGGGGHHHHHHHHHH\x05\x03\x02\x01"|./stack2
+*/
